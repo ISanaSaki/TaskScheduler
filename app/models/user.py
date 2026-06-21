da0ac4ab591ded4afcd6e3b4b2f8ab64a235a74e
ISanaSaki <sanasaki85@gmail.com>
2026-06-21 10:27:36 +0330

    adding email field to user model

--- app/models/user.py
+++ app/models/user.py
@@ -4,4 +4,5 @@ from typing import Optional
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(unique=True, index=True)
+    email: str = Field(unique=True, index=True)
     hashed_password: str
