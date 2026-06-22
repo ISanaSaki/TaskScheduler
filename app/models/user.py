c818875e31f11e61be62f2911e1790ac5e1fdc2a
ISanaSaki <sanasaki85@gmail.com>
2026-06-22 13:33:57 +0330

    adding user table name

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,8 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 
 class User(SQLModel, table=True):
+    __tablename__ = "users"
+    
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(unique=True, index=True)
     email: str = Field(unique=True, index=True)
