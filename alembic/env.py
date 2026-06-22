e848ac36e038bc247e29a9f7639fbbd3185a0fc3
ISanaSaki <sanasaki85@gmail.com>
2026-06-22 13:45:37 +0330

    import user model

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import settings
 from app.models.task import Task, TaskRun
+from app.models.user import User
 from sqlmodel import SQLModel
 import os
 from dotenv import load_dotenv
