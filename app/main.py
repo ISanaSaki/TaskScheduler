1fffe4f92673e43885e485139bac624db4dffa22
ISanaSaki <sanasaki85@gmail.com>
2026-06-22 13:44:52 +0330

    import from correct place

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from contextlib import asynccontextmanager
 from app.core.database import init_db
 from app.api.routes.tasks import router as tasks_router
 from app.api.routes.ws import router as ws_router
-from app.core.scheduler import scheduler,start_scheduler, load_recurring_tasks
+from app.api.routes.auth import router as auth_router
+from app.core.scheduler import scheduler, start_scheduler, load_recurring_tasks
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,7 +13,9 @@ async def lifespan(app: FastAPI):
     start_scheduler()
     yield
     scheduler.shutdown()
-    
+
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
 app.include_router(tasks_router)
 app.include_router(ws_router)
