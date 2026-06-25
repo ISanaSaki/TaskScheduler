f999b1f8517328baf7fee7549e06f1f0b3dcf0c4
ISanaSaki <sanasaki85@gmail.com>
2026-06-25 13:24:37 +0330

    add SMTP settings to config

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,13 @@ class Settings(BaseSettings):
     CELERY_RESULT_BACKEND: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
+    POSTGRES_PASSWORD: str
+    
+    SMTP_HOST: str
+    SMTP_PORT: int = 587
+    SMTP_USER: str
+    SMTP_PASSWORD: str
+    SMTP_FROM: str = "Task Scheduler"
 
     class Config:
         env_file = ".env"
